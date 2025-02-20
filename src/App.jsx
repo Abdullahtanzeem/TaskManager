039c54aa5198d4a2edc800c5d41606999c1c3748
Abdullahtanzeem <abdulltanzz313@gmail.com>
2025-02-20 17:37:15 +0530

    created a function to add task and I can clear them out as well.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { AddTask } from './components/AddTask';
 import { TaskList } from './components/TaskList';
 import './App.css';
 
 function App() {
+  const [tasklist, setTasklist] = useState([]);
   return (
     <div className="App">
       
       <Header />
-      <AddTask />
-      <TaskList />
+      <AddTask tasklist={tasklist} setTasklist= {setTasklist} />
+      <TaskList tasklist={tasklist} setTasklist= {setTasklist}  />
     </div>
   );
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,30 +1,25 @@
 import React from 'react'
 
-export const TaskList = () => {
-    const tasks = [
-        { id: 54493, name: "TASK A", time: "2:09:01 AM 9/14/2025" },
-        { id: 54494, name: "TASK B", time: "2:09:01 AM 9/14/2025" },
-        { id: 54495, name: "TASK C", time: "2:09:01 AM 9/14/2025" }
-
-    ]
+export const TaskList = ({tasklist, setTasklist}) => {
+    
     return (
         <section className="showTask">
             <div className="head">
                 <div>
                     <span className='title'>Todo</span>
-                    <span className='count'>0</span>
+                    <span className='count'>{ tasklist.length }</span>
                 </div>
-                <button className="clearAll">Clear</button>
+                <button className="clearAll" onClick={() => setTasklist([])}>Clear</button>
             </div>
             <ul>
-                {tasks.map((task) => 
-                    <li>
+                {tasklist.map((task) => 
+                    <li key={task.id}>
                     <p>
                         <span className="taskName">{task.name}</span>
                         <span className="time">{task.time}</span>
                     </p>
                     <i className='bi bi-pencil-square'></i>
-                    <i class="bi bi-trash"></i>
+                    <i className="bi bi-trash"></i>
                 </li>
                 )}
                 
